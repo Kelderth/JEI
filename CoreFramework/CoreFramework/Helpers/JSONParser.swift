03b3aa89e78e084a3bcf4af3cdf112393e7d7675
Jesus Francisco Sanchez Mendez <frakcool1993@gmail.com>
2019-02-06 15:52:47 -0500

    Added helper methods to CoreFramework

---
+++ CoreFramework/CoreFramework/Helpers/JSONParser.swift
@@ -0,0 +1,27 @@
+//
+//  JSONParser.swift
+//  CoreFramework
+//
+//  Created by Jesus Sanchez on 2/6/19.
+//  Copyright © 2019 Isai Yepez. All rights reserved.
+//
+
+import Foundation
+
+public class JSONParser {
+    public func decode<T: Decodable>(json: Data, as clazz: T.Type) -> T? {
+        do {
+            let decoder = JSONDecoder()
+            
+            let context = StorageManager.persistentContainer.viewContext
+            decoder.userInfo[CodingUserInfoKey.context!] = context
+            
+            let data = try decoder.decode(T.self, from: json)
+            return data
+        } catch {
+            print("An error occurred while parsing JSON")
+        }
+        
+        return nil
+    }
+}
