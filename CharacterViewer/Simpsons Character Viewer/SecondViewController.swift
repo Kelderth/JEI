eeef43808a868b6200fef0b0f66ab21fa760ba17
Isai Yepez Gómez <isaiyepez@gmail.com>
2019-02-06 11:52:01 -0500

    Sixth commit skeleton workspace

---
+++ CharacterViewer/Simpsons Character Viewer/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Simpsons Character Viewer
+//
+//  Created by Isai Yepez on 2/6/19.
+//  Copyright © 2019 Isai Yepez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
