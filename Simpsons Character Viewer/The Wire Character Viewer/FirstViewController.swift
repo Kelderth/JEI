0a1f551fde096bf6a57bc023a4eb5667f6228748
Isai Yepez Gómez <isaiyepez@gmail.com>
2019-02-06 11:13:36 -0500

    Second refactor, skeleton project

---
+++ Simpsons Character Viewer/The Wire Character Viewer/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  The Wire Character Viewer
+//
+//  Created by Isai Yepez on 2/6/19.
+//  Copyright © 2019 Isai Yepez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
