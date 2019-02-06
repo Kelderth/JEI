f64603aaf1715c280a13f498c12618ed7d0d6d50
Kelderth <Kelderth@gmail.com>
2019-02-06 15:58:56 -0500

    Advances of UI 1.0

---
+++ CharacterViewer/The Wire Character Viewer/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  The Wire Character Viewer
+//
+//  Created by MCS on 2/6/19.
+//  Copyright © 2019 Isai Yepez. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
