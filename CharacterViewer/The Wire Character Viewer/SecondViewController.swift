e9290261e6458a95711871d7c50fd0b8ac6363dc
Kelderth <Kelderth@gmail.com>
2019-02-06 16:23:16 -0500

    The ware UI upgraded

--- CharacterViewer/The Wire Character Viewer/SecondViewController.swift
+++ CharacterViewer/The Wire Character Viewer/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  SecondViewController.swift
 //  Simpsons Character Viewer
 //
 //  Created by Isai Yepez on 2/6/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
