2adb7151e44ba435886b26f2843bc057c4f1317f
Kelderth <Kelderth@gmail.com>
2019-02-07 14:58:33 -0500

    First UI Commit

--- CharacterViewer/The Wire Character Viewer/FirstViewController.swift
+++ CharacterViewer/The Wire Character Viewer/FirstViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var favoriteButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func Favorite(_ sender: UIButton) {
+        var isFavorited: Bool = true
+        
+        
+        if isFavorited == true {
+            favoriteButton.setImage(UIImage(imageLiteralResourceName: "favoriteOn"), for: .normal)
+            isFavorited = false
+        }
+        else {
+            favoriteButton.setImage(UIImage(imageLiteralResourceName: "favoriteOff"), for: .normal)
+            isFavorited = true
+        }
+        print("Favorited!")
+    }
+    
 
 
 }
---
+++ CharacterViewer/The Wire Character Viewer/MasterTableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  MasterTableViewController.swift
+//  The Wire Character Viewer
+//
+//  Created by MCS on 2/6/19.
+//  Copyright © 2019 Isai Yepez. All rights reserved.
+//
+
+import UIKit
+
+class MasterTableViewController: UITableViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 10
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
